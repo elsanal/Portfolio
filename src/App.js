47b648e18c9df84756d4e538823851b28e1ae9b5
elsone <elsanal1995@gmail.com>
2021-09-26 05:55:19 +0800

    version-1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, {Suspense} from 'react'
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Header from './Components/Header'
-import About from './Pages/About'
-import Services from './Pages/Services'
-import Blogs from './Pages/Blogs'
-import BlogDetails from './Pages/Details'
-import Contacts from './Pages/Contacts'
-import Projects from './Pages/Projects'
-import Resume from './Pages/Resume'
-
+import React, { Suspense } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Blogs from "./Pages/Blogs";
+import BlogDetails from "./Pages/Details";
+import Contacts from "./Pages/Contacts";
+import Projects from "./Pages/Projects";
+import Resume from "./Pages/Resume";
 
 function App() {
-
-  var dotenv = require('dotenv')
-  var dotenvExpand = require('dotenv-expand')
-  var myEnv = dotenv.config()
-  dotenvExpand(myEnv)
+  var dotenv = require("dotenv");
+  var dotenvExpand = require("dotenv-expand");
+  var myEnv = dotenv.config();
+  dotenvExpand(myEnv);
 
   return (
     <Suspense fallback="loading">
       <div className="App">
         <Router>
-        <Header/>
-            <Switch>
-                <Route component={About} path="/about" exact/>
-                <Route component={Blogs} path="/" exact/>
-                <Route component={Resume} path="/resume" />
-                <Route component={Services} path="/services" />
-                <Route component={Projects} path="/projects" />
-                <Route component={Blogs} path="/blogs" />
-                <Route component={Contacts} path="/contacts" />
-                <Route component={BlogDetails} path="/blog+details/:id"/>
-            </Switch>
+          <Header />
+          <Switch>
+            <Route component={About} path="/about" exact />
+            <Route component={Blogs} path="/" exact />
+            <Route component={Resume} path="/resume" />
+            <Route component={Services} path="/services" />
+            <Route component={Projects} path="/projects" />
+            <Route component={Blogs} path="/blogs" />
+            <Route component={Contacts} path="/contacts" />
+            <Route component={BlogDetails} path="/blog+details/:id" />
+          </Switch>
         </Router>
       </div>
     </Suspense>
-    
   );
 }
 
